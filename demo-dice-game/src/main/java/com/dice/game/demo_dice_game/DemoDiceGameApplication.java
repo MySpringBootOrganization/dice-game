3c76dea63524ca8a2cf1972f9055dbfc3c2207c7
kcft99 <ktang.consultant@yahoo.com>
2025-07-11 11:25:18 -0400

    initialize demo dice game project

---
+++ demo-dice-game/src/main/java/com/dice/game/demo_dice_game/DemoDiceGameApplication.java
@@ -0,0 +1,13 @@
+package com.dice.game.demo_dice_game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDiceGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoDiceGameApplication.class, args);
+	}
+
+}
---
+++ demo-dice-game/src/test/java/com/dice/game/demo_dice_game/DemoDiceGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dice.game.demo_dice_game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoDiceGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
