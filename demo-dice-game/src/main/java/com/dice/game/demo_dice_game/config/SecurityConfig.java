0c941859f04ba649ae15672a62c26c970a3921d5
KTConsulting <KTConsulting>
2025-08-07 16:40:58 -0400

    Add spring cloud starter security dependency

--- demo-dice-game/src/main/java/com/dice/game/demo_dice_game/config/SecurityConfig.java
+++ demo-dice-game/src/main/java/com/dice/game/demo_dice_game/config/SecurityConfig.java
@@ -2,14 +2,16 @@ package com.dice.game.demo_dice_game.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
-    /* 
+
     @Bean
+    @Profile("azure")
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
             .authorizeHttpRequests(auth -> auth
@@ -17,16 +19,17 @@ public class SecurityConfig {
                 .anyRequest().authenticated()
             )
             .oauth2Login(Customizer.withDefaults())
+            .csrf(csrf -> csrf.disable())
             .logout(logout -> logout.logoutSuccessUrl("/"));
         return http.build();
     }
-        */
-        @Bean
+
+    @Bean
+    @Profile("!azure")
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
             .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
             .csrf(csrf -> csrf.disable());
         return http.build();
     }
-
 }
